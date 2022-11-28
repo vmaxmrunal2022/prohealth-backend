6ea1182fb8d09aeeaf6a04323fd15ebbcda0c0ae
vmaxmahesh <107479074+vmaxmahesh@users.noreply.github.com>
2022-11-28 15:24:25 +0530

    provider controllewr modified

--- app/Http/Controllers/ValidationLists/ProviderController.php
+++ app/Http/Controllers/ValidationLists/ProviderController.php
@@ -12,10 +12,12 @@ class ProviderController extends Controller
 
     public function search(Request $request)
     {
-        $ndc = DB::table('PROVIDER_TYPE_VALIDATION_NAMES')
+        $ndc = DB::table('PHARMACY_VALIDATIONS')
+        ->join('PHARMACY_TABLE', 'PHARMACY_TABLE.PHARMACY_NABP', '=', 'PHARMACY_VALIDATIONS.PHARMACY_NABP')
+
                 // ->select('DIAGNOSIS_VALIDATIONS.DIAGNOSIS_LIST', 'DIAGNOSIS_VALIDATIONS.DIAGNOSIS_ID','DIAGNOSIS_CODES.DESCRIPTION as Description')
-                ->where('PROVIDER_TYPE_VALIDATION_NAMES.PROV_TYPE_LIST_ID', 'like', '%' .strtoupper($request->search). '%')
-                ->orWhere('PROVIDER_TYPE_VALIDATION_NAMES.DESCRIPTION', 'like', '%' . strtoupper($request->search) . '%')
+                // ->where('PHARMACY_LIST', 'like', '%' .strtoupper($request->search). '%')
+                // ->orWhere('PHARMACY_NABP', 'like', '%' . strtoupper($request->search) . '%')
                 ->get();
 
     return $this->respondWithToken($this->token(), '', $ndc);
@@ -25,9 +27,18 @@ class ProviderController extends Controller
 
     public function getProviderList($ndcid)
     {
-        $ndclist = DB::table('PROVIDER_TYPE_VALIDATION_NAMES')
-        ->where('PROV_TYPE_LIST_ID', 'like', '%' . strtoupper($ndcid) . '%')
-                // ->orWhere('EXCEPTION_NAME', 'like', '%' . strtoupper($ndcid) . '%')
+        // $ndclist = DB::table('PROVIDER_TYPE_VALIDATION_NAMES')
+        // ->where('PROV_TYPE_LIST_ID', 'like', '%' . strtoupper($ndcid) . '%')
+        //         // ->orWhere('EXCEPTION_NAME', 'like', '%' . strtoupper($ndcid) . '%')
+        //         ->get();
+
+
+                $ndclist = DB::table('PHARMACY_VALIDATIONS')
+        ->join('PHARMACY_TABLE', 'PHARMACY_TABLE.PHARMACY_NABP', '=', 'PHARMACY_VALIDATIONS.PHARMACY_NABP')
+
+                // ->select('DIAGNOSIS_VALIDATIONS.DIAGNOSIS_LIST', 'DIAGNOSIS_VALIDATIONS.DIAGNOSIS_ID','DIAGNOSIS_CODES.DESCRIPTION as Description')
+                // ->where('PHARMACY_LIST', 'like', '%' .strtoupper($request->search). '%')
+                // ->orWhere('PHARMACY_NABP', 'like', '%' . strtoupper($request->search) . '%')
                 ->get();
 
         return $this->respondWithToken($this->token(), '', $ndclist);
@@ -36,11 +47,10 @@ class ProviderController extends Controller
 
     public function getNDCItemDetails($ndcid)
     {
-        $ndc = DB::table('PROVIDER_TYPE_VALIDATION_NAMES')
-                    ->join('PROVIDER_TYPE_VALIDATIONS', 'PROVIDER_TYPE_VALIDATION_NAMES.PROV_TYPE_LIST_ID', '=', 'PROVIDER_TYPE_VALIDATIONS.PROV_TYPE_LIST_ID')
+        $ndc = DB::table('PHARMACY_TABLE')
                     // ->select('DIAGNOSIS_VALIDATIONS.DIAGNOSIS_LIST', 'DIAGNOSIS_VALIDATIONS.DIAGNOSIS_ID','DIAGNOSIS_CODES.DESCRIPTION as Description','DIAGNOSIS_VALIDATIONS.DIAGNOSIS_STATUS','DIAGNOSIS_VALIDATIONS.PRIORITY')
 
-                    // ->where('NDC_EXCEPTION_LISTS.NDC', 'like', '%' . strtoupper($ndcid) . '%')  
+                    ->where('PHARMACY_NABP', 'like', '%' . strtoupper($ndcid) . '%')  
                     ->first();
 
         return $this->respondWithToken($this->token(), '', $ndc);
