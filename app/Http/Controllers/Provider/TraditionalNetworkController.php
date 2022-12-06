a1d26fd814f0324115df90a796ea2ebac7048e57
vmaxmahesh <107479074+vmaxmahesh@users.noreply.github.com>
2022-12-06 10:29:27 +0530

    traditional network controller modified

--- app/Http/Controllers/Provider/TraditionalNetworkController.php
+++ app/Http/Controllers/Provider/TraditionalNetworkController.php
@@ -16,35 +16,33 @@ class TraditionalNetworkController extends Controller
 
         if ( $request->has( 'new' ) ) {
 
-            $accum_benfit_stat_names = DB::table( 'accum_bene_strategy_names' )->insert(
+
+
+            $accum_benfit_stat_names = DB::table('RX_NETWORK_NAMES')->insert(
                 [
-                    'accum_bene_strategy_id' => strtoupper( $request->accum_bene_strategy_id ),
-                    'accum_bene_strategy_name' => $request->accum_bene_strategy_name,
+                    'network_id' => strtoupper( $request->network_id ),
+                    'network_name' => $request->network_name,
+                    
 
                 ]
             );
 
-            $accum_benfit_stat = DB::table( 'ACCUM_BENEFIT_STRATEGY' )->insert(
+
+            $accum_benfit_stat = DB::table('RX_NETWORKS' )->insert(
                 [
-                    'accum_bene_strategy_id' => strtoupper( $request->accum_bene_strategy_id ),
-                    'pharm_type_variation_ind'=>$request->pharm_type_variation_ind,
-                    'formulary_variation_ind'=>$request->formulary_variation_ind,
-                    'network_part_variation_ind'=>$request->network_part_variation_ind,
-                    'claim_type_variation_ind'=>$request->claim_type_variation_ind,
-                    'date_time_created'=>$createddate,
-                    'user_id'=>'',
-                    'date_time_modified'=>'',
-                    'form_id'=>'',
-                    'user_id_created'=>'',
-                    'accum_exclusion_flag'=>$request->accum_exclusion_flag,
+                    'network_id' => strtoupper( $request->network_id ),
+                    'pharmacy_nabp'=>$request->pharmacy_nabp,
+                    'price_schedule_ovrd'=>$request->price_schedule_ovrd,
                     'effective_date'=>$request->effective_date,
-                    'module_exit'=>'',
-                    'plan_accum_deduct_id'=>$request->plan_accum_deduct_id,
+                    'termination_date'=>$request->termination_date,
 
                 ]
             );
+            $benefitcode = DB::table('RX_NETWORKS')->where('network_id', 'like', $request->network_id )->first();
+
 
         } else {
+
 
             $benefitcode = DB::table('RX_NETWORK_NAMES' )
             ->where( 'network_id', $request->network_id )
@@ -70,11 +68,10 @@ class TraditionalNetworkController extends Controller
                 ]
             );
 
-            $benefitcode = DB::table( 'RX_NETWORKS' )->where( 'pharmacy_nabp', 'like', $request->pharmacy_nabp )->first();
+            $benefitcode = DB::table('RX_NETWORKS')->where('network_id', 'like', $request->network_id )->first();
 
         }
 
-        // $benefitcode = DB::table( 'benefit_codes' )->where( 'benefit_code', 'like', $request->benefit_code )->first();
 
         return $this->respondWithToken( $this->token(), 'Successfully added', $benefitcode );
     }
