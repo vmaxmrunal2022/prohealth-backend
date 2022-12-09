3ca6a34a1b1585455da1d01e410ad24e0bfeadbd
vmaxmahesh <107479074+vmaxmahesh@users.noreply.github.com>
2022-12-09 17:22:23 +0530

    ndc exce

--- app/Http/Controllers/Exception/NDCExceptionController.php
+++ app/Http/Controllers/Exception/NDCExceptionController.php
@@ -8,6 +8,74 @@ use Illuminate\Support\Facades\DB;
 
 class NDCExceptionController extends Controller
 {
+
+    public function add( Request $request ) {
+        $createddate = date( 'y-m-d' );
+
+        if ( $request->has( 'new' ) ) {
+
+
+
+            $accum_benfit_stat_names = DB::table('RX_NETWORK_RULE_NAMES')->insert(
+                [
+                    'rx_network_rule_id' => strtoupper( $request->rx_network_rule_id ),
+                    
+
+                ]
+            );
+
+
+            $accum_benfit_stat = DB::table('RX_NETWORKS' )->insert(
+                [
+                    'network_id' => strtoupper( $request->network_id ),
+                    'pharmacy_nabp'=>$request->pharmacy_nabp,
+                    'effective_date'=>$request->effective_date,
+                    'termination_date'=>$request->termination_date,
+
+                ]
+            );
+            $benefitcode = DB::table('RX_NETWORK_NAMES')->get();
+
+
+        } else {
+
+
+            $benefitcode = DB::table('RX_NETWORK_RULE_NAMES' )
+            ->where( 'rx_network_rule_id', $request->rx_network_rule_id )
+
+
+            ->update(
+                [
+                    'rx_network_rule_id' =>  $request->rx_network_rule_id ,
+                    'rx_network_rule_name'=>$request->rx_network_rule_name,
+
+                ]
+            );
+
+            $accum_benfit_stat = DB::table( 'RX_NETWORK_RULES' )
+            ->where('rx_network_rule_id', $request->rx_network_rule_id )
+            ->update(
+                [
+                    'rx_network_rule_id' => $request->rx_network_rule_id,
+                    'rx_network_rule_id_number'=>$request->rx_network_rule_id_number,
+                    'price_schedule_ovrd'=>$request->price_schedule_ovrd,
+                   'exclude_rule'=>$request->exclude_rule,
+                  
+
+                ]
+            );
+
+            $benefitcode = DB::table('RX_NETWORK_RULES')->where('rx_network_rule_id', 'like', $request->rx_network_rule_id )->first();
+
+        }
+
+
+        return $this->respondWithToken( $this->token(), 'Successfully added',$benefitcode);
+    }
+
+
+
+
     public function search(Request $request)
     {
         $ndc = DB::table('NDC_EXCEPTIONS')
