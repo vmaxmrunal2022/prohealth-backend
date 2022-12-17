fa91b41e4e40565c891915c3f3b42e618a6cba57
shamsh <agoussec@gmail.com>
2022-12-17 12:22:02 +0530

    search audit trial with date is not working

--- app/Http/Controllers/administrator/AuditTrailController.php
+++ app/Http/Controllers/administrator/AuditTrailController.php
@@ -37,31 +37,31 @@ class AuditTrailController extends Controller
 
     public function searchUserLog(Request $request)
     {
-        // print_r($request->all());
-
-        if ($request->from_date != null) {
-            $fromDate = str_replace('-', '', $request->from_date);
-        } else {
-            $fromDate = '';
-        }
         if ($request->to_date != null) {
             $toDate = str_replace('-', '', $request->to_Date);
         } else {
             $toDate = '';
         }
+        if ($request->from_date != null) {
+            $fromDate = str_replace('-', '', $request->from_date);
+        } else {
+            $fromDate = '';
+        }
+
         // print($fromDate);
-        // print((date('Y-m-d', $fromDate)));
+        // print($toDate);
+        // $countries = DB::table('COUNTRY_STATES')->where(DB::raw('UPPER(DESCRIPTION)'), 'like','%'.strtoupper($c_id).'%')->get();
         $search = DB::table('FE_RECORD_LOG')
             ->where('table_name', $request->table_name['value'])
-            ->Where('user_id', $request->user_id['uvalue'])
-            ->Where('record_action', $request->record_action['rvalue'])
-            //   ->Where('DATE_CREATED', '>=', $fromDate)
-            //   ->Where('DATE_CREATED', '<=' ,$toDate)
-            // ->Where(date('Y-m-d','DATE_CREATED'), '>=', $fromDate)
-            // ->Where(date('Y-m-d','DATE_CREATED'), '<=', $toDate)
+            ->where('user_id', $request->user_id['uvalue'])
+            ->where('record_action', $request->record_action['rvalue'])
+            // ->where('DATE_CREATED', '>=', $fromDate)
+            // ->where('DATE_CREATED', '<=', $toDate)
+            // ->Where(DB::raw('date("Y-m-d", DATE_CREATED)'), '>=', $request->from_date)
+            // ->Where(DB::raw('date("Y-m-d", DATE_CREATED)'), '<=', $request->to_date)
             // ->whereBetween('DATE_CREATED',[$fromDate, $toDate])
             ->get();
-        //   print_r($search);
+        // print_r($search);
 
         return $this->respondWithToken($this->token(), '', $search);
     }
