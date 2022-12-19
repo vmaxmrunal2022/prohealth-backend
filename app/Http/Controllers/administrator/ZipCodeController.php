94c548cc0eb4a869765de74603a44ff3b248cecb
prasheesh <prasheesh.t@vmaxindia.com>
2022-12-19 10:51:50 +0530

    zip controller

--- app/Http/Controllers/administrator/ZipCodeController.php
+++ app/Http/Controllers/administrator/ZipCodeController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Contracts\Encryption\DecryptException;
 
 class ZipCodeController extends Controller
 {
@@ -30,7 +32,8 @@ class ZipCodeController extends Controller
     }
 
     public function submitFormData(Request $request){
-
+        $requesta = Crypt::decryptString('U2FsdGVkX18vhdbpIbCdrKAwHeUevqKu2higsiJ2izHjGg0v8trOx4ywt/QDxN5RQUVfqbM9L0Q/zWjauQhk1perZPqf+uoerdm3/nF2iuPIJoKeyhs1gtTYzif676APDVuaD9flH8X4hFaLVGL7oOe83a+2GN9mnTjYnm/Rupsfvtjv1bzZbauOvan5uGe7TrdzCS8j5+rNUZc1cGCxFture3NCEjxPAi8+f2r0bnMXhr0PcDRx81ig1caS2NoiqgIaby98WA6R4+Fncbpqjt2m8D+IZ9m9nzsOuam9eO6ojVPzUIZmBSEI36ONZCFK5MehA9Qq320JpMjGfxPF7c0B0cds9uoC2uytKMmUr6pLo8pVjPM09ZbCyX4Bmwk0ztpeAOMn8B6CJTyO1wWxMxNhW6CLzf8aelp6s1K+VHKHUoVjlulgA9apcV++7J/S');
+        print_r($requesta); exit;
         if ($request->has('new')) {
             $addUser = DB::table('ZIP_CODES')
                 ->insert([
