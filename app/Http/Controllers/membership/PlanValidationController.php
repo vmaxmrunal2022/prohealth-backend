4488c7345fdd194926a7de373f6676fed78eeeff
vmaxmrunal2022 <110972208+vmaxmrunal2022@users.noreply.github.com>
2023-02-22 13:09:30 +0530

    changes in PlanValidationController

--- app/Http/Controllers/membership/PlanValidationController.php
+++ app/Http/Controllers/membership/PlanValidationController.php
@@ -5,18 +5,26 @@ namespace App\Http\Controllers\membership;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class PlanValidationController extends Controller
 {
   public function get(Request $request)
   {
-    $planValidation = DB::table('PLAN_VALIDATION_LISTS')
-      ->join('CUSTOMER', 'CUSTOMER.customer_id', '=', 'PLAN_VALIDATION_LISTS.customer_id')
-      ->where('CUSTOMER.customer_id', 'like', '%' . strtoupper($request->search) . '%')
-      //   ->orWhere('PLAN_VALIDATION_LISTS.client_id', 'like', '%'. strtoupper($request->search) .'%')
-      ->orWhere('CUSTOMER.customer_name', 'like', '%' . strtoupper($request->search) . '%')
-      ->get();
-    return $this->respondWithToken($this->token(), '', $planValidation);
+    $validator = Validator::make($request->all(), [
+      "search" => ['required']
+    ]);
+    if ($validator->fails()) {
+      return $this->respondWithToken($this->token(), $validator->errors(), $validator->errors(), "false");
+    } else {
+      $planValidation = DB::table('PLAN_VALIDATION_LISTS')
+        ->join('CUSTOMER', 'CUSTOMER.customer_id', '=', 'PLAN_VALIDATION_LISTS.customer_id')
+        ->where('CUSTOMER.customer_id', 'like', '%' . strtoupper($request->search) . '%')
+        //   ->orWhere('PLAN_VALIDATION_LISTS.client_id', 'like', '%'. strtoupper($request->search) .'%')
+        ->orWhere('CUSTOMER.customer_name', 'like', '%' . strtoupper($request->search) . '%')
+        ->get();
+      return $this->respondWithToken($this->token(), '', $planValidation);
+    }
   }
 
   public function getClientDetails(Request $request)
@@ -34,42 +42,42 @@ class PlanValidationController extends Controller
   {
 
     $getEligibilityData = DB::table('plan_validation_lists')
-        ->where('customer_id',strtoupper($request->customer_id))
-        ->where('client_id',strtoupper($request->client_id))
-        ->where('client_group_id', strtoupper($request->client_group_id))
-        ->where('plan_id', $request->plan_id)
-        ->first();
+      ->where('customer_id', strtoupper($request->customer_id))
+      ->where('client_id', strtoupper($request->client_id))
+      ->where('client_group_id', strtoupper($request->client_group_id))
+      ->where('plan_id', $request->plan_id)
+      ->first();
 
 
-    if ($request->add_new==1) {
+    if ($request->add_new == 1) {
 
-      if($getEligibilityData){
+      if ($getEligibilityData) {
 
         return $this->respondWithToken($this->token(), 'This record already exists in the system..!!!', $getEligibilityData);
-
-
-      }else{
+      } else {
 
         $planValidation = DB::table('plan_validation_lists')
-        ->insert([
-          'customer_id' => $request->customer_id,
-          'client_id' => $request->client_id,
-          'client_group_id' => $request->client_group_id,
-          'plan_id' => $request->plan_id
-        ]);
-      return $this->respondWithToken($this->token(), 'Record Added Successfully !', $planValidation);
-
+          ->insert([
+            'customer_id' => $request->customer_id,
+            'client_id' => $request->client_id,
+            'client_group_id' => $request->client_group_id,
+            'plan_id' => $request->plan_id
+          ]);
+        return $this->respondWithToken($this->token(), 'Record Added Successfully !', $planValidation);
       }
-     
-    } else if($request->add_new==0){
+    } else if ($request->add_new == 0) {
       $planValidation = DB::table('plan_validation_lists')
         ->where('customer_id', $request->customer_id)
-        ->where('client_id',$request->client_id)
-        ->where('client_group_id',$request->client_group_id)
+        ->where('client_id', $request->client_id)
+        ->where('client_group_id', $request->client_group_id)
+        ->where('plan_id', $request->plan_id)
         ->update([
           // 'customer_id' => $request->customer_id,
-          'plan_id' => $request->plan_id
+          'plan_id' => $request->plan_id,
+          'date_time_modified' => date('Y-m-d H:i:s'),
         ]);
+      // ->get();
+      // dd($planValidation);
       return $this->respondWithToken($this->token(), 'Record Updated Successfully !', $planValidation);
     }
   }
@@ -81,5 +89,4 @@ class PlanValidationController extends Controller
       ->get();
     return $this->respondWithToken($this->token(), '', $plan_ids);
   }
-
 }
