1e0479d32a90bbdfc280361b04508f32ea929abd
vmaxmahesh <vmax.mahesh@gmail.com>
2023-02-27 18:10:39 +0530

    super benefit

--- app/Http/Controllers/Exception/SuperBenefitControler.php
+++ app/Http/Controllers/Exception/SuperBenefitControler.php
@@ -18,36 +18,54 @@ class SuperBenefitControler extends Controller
         $terminate_date = date('Ymd', strtotime($request->termination_date));
 
 
+        $recordcheck = DB::table('SUPER_BENEFIT_LIST_NAMES')
+        ->where('super_benefit_list_id', strtoupper($request->super_benefit_list_id))
+        ->first();
+
+
         if ( $request->has( 'new' ) ) {
 
+            if($recordcheck){
+                return $this->respondWithToken($this->token(), 'Super Benefit List Id  already exists in the system..!!!', $recordcheck,208);
 
-
-            $accum_benfit_stat_names = DB::table('SUPER_BENEFIT_LIST_NAMES')->insert(
-                [
-                    'super_benefit_list_id' => strtoupper( $request->super_benefit_list_id ),
-                    'description'=>$request->description
-                    
-
-                ]
-            );
-
-
-            $accum_benfit_stat = DB::table('SUPER_BENEFIT_LISTS' )->insert(
-                [
-                    'super_benefit_list_id'=>strtoupper( $request->super_benefit_list_id ),
-
-                    'benefit_list_id'=>$request->benefit_list_id,
-                    'accum_benefit_strategy_id'=>$request->accum_benefit_strategy_id,
-                    'effective_date'=>$effective_date,
-                    'termination_date'=>$terminate_date,
-
-
-                ]
-            );
-
-            if ($accum_benfit_stat) {
-                return $this->respondWithToken($this->token(), 'Recored Added Successfully', $accum_benfit_stat);
             }
+
+            else{
+
+
+                $accum_benfit_stat_names = DB::table('SUPER_BENEFIT_LIST_NAMES')->insert(
+                    [
+                        'super_benefit_list_id' => strtoupper( $request->super_benefit_list_id ),
+                        'description'=>$request->description
+                        
+    
+                    ]
+                );
+    
+    
+                $accum_benfit_stat = DB::table('SUPER_BENEFIT_LISTS' )->insert(
+                    [
+                        'super_benefit_list_id'=>strtoupper( $request->super_benefit_list_id ),
+    
+                        'benefit_list_id'=>$request->benefit_list_id,
+                        'accum_benefit_strategy_id'=>$request->accum_benefit_strategy_id,
+                        'effective_date'=>$effective_date,
+                        'termination_date'=>$terminate_date,
+    
+    
+                    ]
+                );
+    
+                if ($accum_benfit_stat) {
+                    return $this->respondWithToken($this->token(), 'Recored Added Successfully', $accum_benfit_stat);
+                }
+
+            }
+
+
+
+
+         
         } else {
 
 
