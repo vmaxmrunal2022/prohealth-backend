66adc8841ecf675ccaeaef0f56f215275364d564
vmaxmahesh <vmax.mahesh@gmail.com>
2023-03-06 13:17:23 +0530

    flexi

--- app/Http/Controllers/Provider/FlexibleNetworkController.php
+++ app/Http/Controllers/Provider/FlexibleNetworkController.php
@@ -16,53 +16,68 @@ class FlexibleNetworkController extends Controller
 
         if ( $request->add_new==1) {
 
+            if($recordCheck){
 
-            $accum_benfit_stat_names = DB::table('RX_NETWORK_RULE_NAMES')->insert(
-                [
-                    'rx_network_rule_id' => strtoupper( $request->rx_network_rule_id ),
-                    'rx_network_rule_name'=>strtoupper( $request->rx_network_rule_name ),
-                    'gpi_exception_list_ovrd'=>$request->gpi_exception_list_ovrd,
-                    'ndc_exception_list_ovrd'=>$request->ndc_exception_list_ovrd,
-                    'default_comm_charge_paid'=>$request->default_comm_charge_paid,
-                    'default_comm_charge_reject'=>$request->default_comm_charge_reject,
-                    'min_rx_qty'=>$request->min_rx_qty,
-                    'max_rx_qty'=>$request->max_rx_qty,
-                    'min_rx_days'=>$request->min_rx_days,
-                    'max_rx_days'=>$request->max_rx_days,
-                    'max_retail_fills'=>$request->max_retail_fills,
-                    'max_fills_opt'=>$request->max_fills_opt,
-                    'starter_dose_days'=>$request->starter_dose_days,
-                    'starter_dose_bypass_days'=>$request->starter_dose_bypass_days,
-                    'starter_dose_maint_bypass_days'=>$request->starter_dose_maint_bypass_days,
-                    'pricing_ovrd_list_id'=>$request->pricing_ovrd_list_id,
-                    
-
-                ]
-            );
+                return $this->respondWithToken($this->token(), 'Record Already Exists', $recordCheck);
 
 
-            $accum_benfit_stat = DB::table('RX_NETWORK_RULES' )->insert(
-                [
-                    'rx_network_rule_id' => strtoupper( $request->rx_network_rule_id ),
-                    'rx_network_rule_id_number'=>$request->rx_network_rule_id_number,
-                    'pharmacy_chain'=>$request->pharmacy_chain,
-                    'state'=>$request->state,
-                    'county'=>$request->county,
-                    'zip_code'=>$request->zip_code,
-                    'area_code'=>$request->area_code,
-                    'price_schedule_ovrd'=>$request->price_schedule_ovrd,
-                    'exclude_rule'=>$request->exclude_rule,
-                    'effective_date'=>$request->effective_date,
-                    'termination_date'=>$request->termination_date,
-                    'exchange_code'=>$request->exchange_code,
-                    'pharmacy_status'=>$request->pharmacy_status,
-                  
+            }
 
-                ]
-            );
+            else{
 
-            $benefitcode = DB::table( 'RX_NETWORK_RULE_NAMES' )->where( 'rx_network_rule_id', 'like', $request->rx_network_rule_id )->first();
+                $accum_benfit_stat_names = DB::table('RX_NETWORK_RULE_NAMES')->insert(
+                    [
+                        'rx_network_rule_id' => strtoupper( $request->rx_network_rule_id ),
+                        'rx_network_rule_name'=>strtoupper( $request->rx_network_rule_name ),
+                        'gpi_exception_list_ovrd'=>$request->gpi_exception_list_ovrd,
+                        'ndc_exception_list_ovrd'=>$request->ndc_exception_list_ovrd,
+                        'default_comm_charge_paid'=>$request->default_comm_charge_paid,
+                        'default_comm_charge_reject'=>$request->default_comm_charge_reject,
+                        'min_rx_qty'=>$request->min_rx_qty,
+                        'max_rx_qty'=>$request->max_rx_qty,
+                        'min_rx_days'=>$request->min_rx_days,
+                        'max_rx_days'=>$request->max_rx_days,
+                        'max_retail_fills'=>$request->max_retail_fills,
+                        'max_fills_opt'=>$request->max_fills_opt,
+                        'starter_dose_days'=>$request->starter_dose_days,
+                        'starter_dose_bypass_days'=>$request->starter_dose_bypass_days,
+                        'starter_dose_maint_bypass_days'=>$request->starter_dose_maint_bypass_days,
+                        'pricing_ovrd_list_id'=>$request->pricing_ovrd_list_id,
+                        
+    
+                    ]
+                );
+    
+    
+                $accum_benfit_stat = DB::table('RX_NETWORK_RULES' )->insert(
+                    [
+                        'rx_network_rule_id' => strtoupper( $request->rx_network_rule_id ),
+                        'rx_network_rule_id_number'=>$request->rx_network_rule_id_number,
+                        'pharmacy_chain'=>$request->pharmacy_chain,
+                        'state'=>$request->state,
+                        'county'=>$request->county,
+                        'zip_code'=>$request->zip_code,
+                        'area_code'=>$request->area_code,
+                        'price_schedule_ovrd'=>$request->price_schedule_ovrd,
+                        'exclude_rule'=>$request->exclude_rule,
+                        'effective_date'=>$request->effective_date,
+                        'termination_date'=>$request->termination_date,
+                        'exchange_code'=>$request->exchange_code,
+                        'pharmacy_status'=>$request->pharmacy_status,
+                      
+    
+                    ]
+                );
 
+                if($accum_benfit_stat){
+
+                    return $this->respondWithToken($this->token(), 'Record Added Successfully', $accum_benfit_stat);
+
+
+                }
+    
+
+            }
 
 
         } else {
@@ -101,13 +116,15 @@ class FlexibleNetworkController extends Controller
 
                 ]
             );
+            if($accum_benfit_stat){
+                return $this->respondWithToken( $this->token(), 'Record Updated Successfully',$accum_benfit_stat);
 
-            $benefitcode = DB::table('RX_NETWORK_RULES')->where('rx_network_rule_id', 'like', $request->rx_network_rule_id )->first();
+            }
+
 
         }
 
 
-        return $this->respondWithToken( $this->token(), 'Successfully added',$benefitcode);
     }
 
 
@@ -116,12 +133,9 @@ class FlexibleNetworkController extends Controller
     {
            
              $ndc = DB::table('RX_NETWORK_RULE_NAMES')
-        ->join('RX_NETWORK_RULES', 'RX_NETWORK_RULE_NAMES.RX_NETWORK_RULE_ID', '=', 'RX_NETWORK_RULE_NAMES.RX_NETWORK_RULE_ID')
-        ->where('RX_NETWORK_RULES.PHARMACY_CHAIN',$request->pharmacy_chain)->get();
-
-
-
-    return $this->respondWithToken($this->token(), '', $ndc);
+             ->join('RX_NETWORK_RULES', 'RX_NETWORK_RULE_NAMES.RX_NETWORK_RULE_ID', '=', 'RX_NETWORK_RULE_NAMES.RX_NETWORK_RULE_ID')
+            ->where('RX_NETWORK_RULES.PHARMACY_CHAIN',$request->pharmacy_chain)->get();
+             return $this->respondWithToken($this->token(), '', $ndc);
     }
 
 
