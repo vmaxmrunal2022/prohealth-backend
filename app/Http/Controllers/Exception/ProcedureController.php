36e828f1c9e7712a09c20c5a9fbd2bf88ddf8a58
vmaxmahesh <vmax.mahesh@gmail.com>
2023-03-20 19:27:55 +0530

    procedure Controller added some fields

--- app/Http/Controllers/Exception/ProcedureController.php
+++ app/Http/Controllers/Exception/ProcedureController.php
@@ -17,17 +17,18 @@ class ProcedureController extends Controller
         if ( $request->has( 'new' ) ) {
 
             $exist = DB::table('PROCEDURE_EXCEPTION_LISTS')
-            ->where('procedure_exception_list',$request->procedure_exception_list)->get();
-            if($exist->count() > 0){
-                return $this->respondWithToken( $this->token(), 'Procedure List ID Already Existed',[]);
+            ->where('procedure_exception_list',strtoupper($request->procedure_exception_list))->first();
+
+            if($exist){
+                return $this->respondWithToken( $this->token(), 'Procedure List ID Already Existed');
             }
 
 
             $accum_benfit_stat_names = DB::table('PROCEDURE_EXCEPTION_NAMES')->insert(
                 [
-                    'procedure_exception_list' => strtoupper( $request->procedure_exception_list ),
+                    'procedure_exception_list' => strtoupper($request->procedure_exception_list),
 
-                    // 'exception_name'=>$request->exception_name,
+                    'exception_name'=>$request->exception_name,
                     
 
                 ]
@@ -35,15 +36,13 @@ class ProcedureController extends Controller
 
             $accum_benfit_stat = DB::table('PROCEDURE_EXCEPTION_LISTS' )->insert(
                 [
-                    'procedure_exception_list'=>$request->procedure_exception_list,
+                    'procedure_exception_list'=>strtoupper($request->procedure_exception_list),
                     'accum_bene_strategy_id'=>$request->accum_bene_strategy_id,
                     'benefit_code'=>$request->benefit_code,
                     'copay_strategy_id'=>$request->copay_strategy_id,
                     'coverage_start_days'=>$request->coverage_start_days,
                     'diagnosis_id'=>$request->diagnosis_id,
                     'diagnosis_list'=>$request->diagnosis_list,
-                    // 'exception_list'=>$request->exception_list,
-                    // 'exception_name'=>$request->exception_name,
                     'max_age'=>$request->max_age,
                     'max_price'=>$request->max_price,
                     'max_price_opt'=>$request->max_price_opt,
@@ -53,7 +52,6 @@ class ProcedureController extends Controller
                     'physician_specialty_list'=>$request->physician_specialty_list,
                     'pricing_strategy_id'=>$request->pricing_strategy_id,
                     'proc_code_list_id'=>$request->proc_code_list_id,
-                    // 'procedure_exception_list'=>$request->procedure_exception_list,
                     'process_rule'=>$request->process_rule,
                     'provider_type'=>$request->provider_type,
                     'reject_only_msg_flag'=>$request->reject_only_msg_flag,
@@ -65,40 +63,48 @@ class ProcedureController extends Controller
                     'effective_date' => $request->effective_date,
                     'termination_date' => $request->termination_date,
                     'message_stop_date' => $request->message_stop_date,
+                    'message'=>$request->message,
+                    'min_price'=>$request->min_price,
+                    'max_age'=>$request->max_age,
+                    'min_age'=>$request->min_age,
+                    'max_qty_over_time'=>$request->max_qty_over_time,
+                    'ucr'=>$request->ucr,
                 ]
             );
-            $benefitcode = DB::table('PROCEDURE_EXCEPTION_LISTS')->where('procedure_exception_list', 'like', '%'.$request->procedure_exception_list .'%')->first();
+            return $this->respondWithToken( $this->token(), 'Record Added Successfully',$accum_benfit_stat);
 
 
         } else {
 
-
-            // dd($request->all())
-
-            $benefitcode = DB::table('PROCEDURE_EXCEPTION_LISTS')
-            ->where('procedure_exception_list',$request->procedure_exception_list)
+            $exceptipon_names = DB::table('PROCEDURE_EXCEPTION_NAMES')
+            ->where('procedure_exception_list',strtoupper($request->procedure_exception_list))
             // ->where('new_claim_status', $request->new_claim_status )
             ->update(
                 [
-                    // 'procedure_exception_list'=>$request->procedure_exception_list,
+                    'exception_name'=>$request->exception_name,
+
+                ]);
+
+
+            $exception = DB::table('PROCEDURE_EXCEPTION_LISTS')
+            ->where('procedure_exception_list',strtoupper($request->procedure_exception_list))
+            ->update(
+                [
                     'accum_bene_strategy_id'=>$request->accum_bene_strategy_id,
                     'benefit_code'=>$request->benefit_code,
                     'copay_strategy_id'=>$request->copay_strategy_id,
                     'coverage_start_days'=>$request->coverage_start_days,
                     'diagnosis_id'=>$request->diagnosis_id,
                     'diagnosis_list'=>$request->diagnosis_list,
-                    // 'exception_list'=>$request->exception_list,
-                    // 'exception_name'=>$request->exception_name,
                     'max_age'=>$request->max_age,
                     'max_price'=>$request->max_price,
                     'max_price_opt'=>$request->max_price_opt,
                     'module_exit'=>$request->module_exit,
-                    // 'new_claim_status'=>$request->new_claim_status,
+                    'new_claim_status'=>$request->new_claim_status,
                     'physician_list'=>$request->physician_list,
                     'physician_specialty_list'=>$request->physician_specialty_list,
                     'pricing_strategy_id'=>$request->pricing_strategy_id,
                     'proc_code_list_id'=>$request->proc_code_list_id,
-                    // 'procedure_exception_list'=>$request->procedure_exception_list,
                     'process_rule'=>$request->process_rule,
                     'provider_type'=>$request->provider_type,
                     'reject_only_msg_flag'=>$request->reject_only_msg_flag,
@@ -109,32 +115,23 @@ class ProcedureController extends Controller
                     'valid_relation_code'=>$request->valid_relation_code,
                     'effective_date' => $request->effective_date,
                     'termination_date' => $request->termination_date,
-                    'message_stop_date' => $request->message_stop_date
-                
+                    'message_stop_date' => $request->message_stop_date,
+                    'message'=>$request->message,
+                    'min_price'=>$request->min_price,
+                    'max_age'=>$request->max_age,
+                    'min_age'=>$request->min_age,
+                    'max_qty_over_time'=>$request->max_qty_over_time,
+                    'ucr'=>$request->ucr,
 
                 ]
             );
 
-            $benefitcode = DB::table('PROCEDURE_EXCEPTION_LISTS')->where('new_claim_status', 'like', '%'.$request->new_claim_status .'%')->first();
+            return $this->respondWithToken( $this->token(), 'Record Updated Successfully',$exception);
 
-
-            // $accum_benfit_stat = DB::table('PROCEDURE_EXCEPTION_NAMES' )
-            // ->where('procedure_exception_list', $request->procedure_exception_list )
-            // ->update(
-            //     [
-            //         // 'exception_name'=>$request->exception_name,
-                   
-                  
-
-            //     ]
-            // );
-
-            // $benefitcode = DB::table('PROCEDURE_EXCEPTION_NAMES')->where('proc_code_list', 'like', '%'.$request->proc_code_list .'%')->first();
 
         }
 
 
-        return $this->respondWithToken( $this->token(), 'Successfully added',$benefitcode);
     }
 
 
