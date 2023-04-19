6a8ba26cbac961356692d2f7d1c04422e17262e6
vmaxmahesh <vmax.mahesh@gmail.com>
2023-04-19 11:30:36 +0530

    prior auth added

--- app/Http/Controllers/membership/PriorAuthController.php
+++ app/Http/Controllers/membership/PriorAuthController.php
@@ -16,7 +16,10 @@ class PriorAuthController extends Controller
                          ->orWhere('customer_id', 'like', '%'. strtoupper($request->search) .'%')
                          ->orWhere('client_id', 'like', '%'. strtoupper($request->search) .'%')
                          ->orWhere('client_group_id', 'like', '%'. strtoupper($request->search) .'%')
+                         ->orWhere('prior_auth_code_num', 'like', '%'. strtoupper($request->search) .'%')
+
                          ->get();
+
 
         return $this->respondWithToken($this->token(), '', $priorAuthList);
     }
@@ -74,7 +77,34 @@ class PriorAuthController extends Controller
                                 'price_sched_ovr' => $request->price_sched_ovr,
                                 'copay_sched_ovr' => $request->copay_sched_ovr,
                                 'accum_bene_exclude_flag' => $request->accum_bene_exclude_flag,
-                                'provider_type' => $request->provider_type,                              
+                                'provider_type' => $request->provider_type,   
+                                
+                                'accum_bene_error_cat_ovr' => $request->accum_bene_error_cat_ovr,
+                                'benefit_code' => $request->benefit_code,
+                                'brand_copay_amt' => $request->brand_copay_amt,
+                                'days_supply_error_cat_ovr' => $request->days_supply_error_cat_ovr,
+                                'diagnosis_id' => $request->diagnosis_id,
+                                'drug_error_cat_ovr' => $request->drug_error_cat_ovr,
+                                'elig_error_cat_ovr' => $request->elig_error_cat_ovr,
+                                'generic_copay_amt' => $request->generic_copay_amt,
+                                'max_daily_dose' => $request->max_daily_dose,
+                                'max_days' => $request->max_days,
+                                'max_dollar_amt' => $request->max_dollar_amt,
+                                'max_num_fills' => $request->max_num_fills,
+                                'max_quantity' => $request->max_quantity,
+                                'num_fills_used' => $request->num_fills_used,
+                                'oltp_date_used' => $request->oltp_date_used,
+                                'patient_paid_diff_flag'=> $request->patient_paid_diff_flag,
+                                'pharm_error_cat_ovr' => $request->pharm_error_cat_ovr,
+                                'pharmacy_nabp' => $request->pharmacy_nabp,
+                                'pharmacy_status_override' => $request->pharmacy_status_override,
+                                'phy_error_cat_ovr' => $request->phy_error_cat_ovr,
+                                'prior_auth_basis_type' => $request->prior_auth_basis_type,
+                                'prior_auth_note' => $request->prior_auth_note,
+                                'procedure_code' => $request->procedure_code,
+                                'qty_error_cat_ovr' => $request->qty_error_cat_ovr,
+                                'refill_error_cat_ovr' => $request->refill_error_cat_ovr,
+                                'service_type' => $request->service_type,
                             ]);
 
             }
@@ -114,6 +144,33 @@ class PriorAuthController extends Controller
                                 'copay_sched_ovr' => $request->copay_sched_ovr,
                                 'accum_bene_exclude_flag' => $request->accum_bene_exclude_flag,
                                 'provider_type' => $request->provider_type,  
+
+                                'accum_bene_error_cat_ovr' => $request->accum_bene_error_cat_ovr,
+                                'benefit_code' => $request->benefit_code,
+                                'brand_copay_amt' => $request->brand_copay_amt,
+                                'days_supply_error_cat_ovr' => $request->days_supply_error_cat_ovr,
+                                'diagnosis_id' => $request->diagnosis_id,
+                                'drug_error_cat_ovr' => $request->drug_error_cat_ovr,
+                                'elig_error_cat_ovr' => $request->elig_error_cat_ovr,
+                                'generic_copay_amt' => $request->generic_copay_amt,
+                                'max_daily_dose' => $request->max_daily_dose,
+                                'max_days' => $request->max_days,
+                                'max_dollar_amt' => $request->max_dollar_amt,
+                                'max_num_fills' => $request->max_num_fills,
+                                'max_quantity' => $request->max_quantity,
+                                'num_fills_used' => $request->num_fills_used,
+                                'oltp_date_used' => $request->oltp_date_used,
+                                'patient_paid_diff_flag'=> $request->patient_paid_diff_flag,
+                                'pharm_error_cat_ovr' => $request->pharm_error_cat_ovr,
+                                'pharmacy_nabp' => $request->pharmacy_nabp,
+                                'pharmacy_status_override' => $request->pharmacy_status_override,
+                                'phy_error_cat_ovr' => $request->phy_error_cat_ovr,
+                                'prior_auth_basis_type' => $request->prior_auth_basis_type,
+                                'prior_auth_note' => $request->prior_auth_note,
+                                'procedure_code' => $request->procedure_code,
+                                'qty_error_cat_ovr' => $request->qty_error_cat_ovr,
+                                'refill_error_cat_ovr' => $request->refill_error_cat_ovr,
+                                'service_type' => $request->service_type,
                                  //Pricing/ Misc tab ends                            
                             ]);
             return $this->respondWithToken($this->token(),'Record Updated Successfully!', $update);
