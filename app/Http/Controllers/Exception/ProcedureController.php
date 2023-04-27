4aa5cd03f51b41d1b8432dce9fce7b5bc6982148
vmaxmahesh <vmax.mahesh@gmail.com>
2023-04-27 15:36:20 +0530

    procedure exception api modified

--- app/Http/Controllers/Exception/ProcedureController.php
+++ app/Http/Controllers/Exception/ProcedureController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers\Exception;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class ProcedureController extends Controller
 {
 
 
-    public function add( Request $request ) {
+    public function addcopy( Request $request ) {
 
         $createddate = date( 'y-m-d' );
 
@@ -34,7 +36,8 @@ class ProcedureController extends Controller
                 ]
             );
 
-            $accum_benfit_stat = DB::table('PROCEDURE_EXCEPTION_LISTS' )->insert(
+            $accum_benfit_stat = DB::table('PROCEDURE_EXCEPTION_LISTS' )
+            ->insert(
                 [
                     'procedure_exception_list'=>strtoupper($request->procedure_exception_list),
                     'accum_bene_strategy_id'=>$request->accum_bene_strategy_id,
@@ -131,6 +134,319 @@ class ProcedureController extends Controller
         }
 
 
+    }
+
+    public function add(Request $request)
+    {
+        $createddate = date( 'y-m-d' );
+
+        $validation = DB::table('PROCEDURE_EXCEPTION_NAMES')
+        ->where('procedure_exception_list',$request->procedure_exception_list)
+        ->get();
+
+        if ($request->add_new == 1) {
+
+            $validator = Validator::make($request->all(), [
+                'procedure_exception_list' => ['required', 'max:10', Rule::unique('PROCEDURE_EXCEPTION_NAMES')->where(function ($q) {
+                    $q->whereNotNull('procedure_exception_list');
+                })],
+                // 'ndc' => ['required', 'max:11', Rule::unique('NDC_EXCEPTION_LISTS')->where(function ($q) {
+                //     $q->whereNotNull('NDC');
+                // })],
+
+                // 'effective_date' => ['required', 'max:10', Rule::unique('NDC_EXCEPTION_LISTS')->where(function ($q) {
+                //     $q->whereNotNull('effective_date');
+                // })],
+
+                // 'ndc_exception_list' => ['required', 'max:10', Rule::unique('NDC_EXCEPTIONS')->where(function ($q) {
+                //     $q->whereNotNull('ndc_exception_list');
+                // })],
+
+                "exception_name" => ['max:36'],
+                "NEW_CLAIM_STATUS"=>['max:2'],
+                "SERVICE_TYPE"=>['max:1'],
+                'PROCESS_RULE'=>['max:15','min:5'],
+                'PHYSICIAN_LIST'=>['max:10'],
+                'PHYSICIAN_SPECIALTY_LIST'=>['max:10'],
+                'DIAGNOSIS_LIST'=>['max:10'],
+                'PRICING_STRATEGY_ID'=>['max:11'],
+                'ACCUM_BENE_STRATEGY_ID'=>['max:11'],
+                'COPAY_STRATEGY_ID'=>['max:10'],
+                'MESSAGE'=>['max:10'],
+                'MESSAGE_STOP_DATE'=>['max:10'],
+                'MIN_AGE'=>['max:6'],
+                'MAX_AGE'=>['max:6'],
+                'MIN_PRICE'=>['max:6'],
+                'MAX_PRICE'=>['max:6'],
+                'MIN_PRICE_OPT'=>['max:6'],
+                'MAX_PRICE_OPT'=>['max:6'],
+                'VALID_RELATION_CODE'=>['max:6'],
+                'SEX_RESTRICTION'=>['max:6'],
+                'MODULE_EXIT'=>['max:6'],
+                'DATE_TIME_CREATED'=>['max:6'],
+                'USER_ID_CREATED'=>['max:6'],
+                'DATE_TIME_MODIFIED'=>['min:2','max:12'],
+                'USER_ID'=>['min:2','max:12'],
+                'EFFECTIVE_DATE'=>['max:6'],
+                'TERMINATION_DATE'=>['max:12','min:2'],
+                'REJECT_ONLY_MSG_FLAG'=>['max:12','min:2'],
+                'MAX_QTY_OVER_TIME'=>['max:12|min:2'],
+                'COVERAGE_START_DAYS'=>['max:6'],
+                'BENEFIT_CODE'=>['max:6'],
+                'SERVICE_MODIFIER'=>['max:2'],
+                'DIAGNOSIS_ID'=>['max:1'],
+                'MAX_RX_QTY_OPT'=>['max:1'],
+                'PROVIDER_TYPE'=>['max:1'],
+                'UCR'=>['numeric|max:6'],
+                'PROC_CODE_LIST_ID'=>['numeric|max:6'],
+                'RX_QTY_OPT_MULTIPLIER'=>['numeric|max:6'],
+
+
+            ]);
+
+            if ($validator->fails()) {
+                return $this->respondWithToken($this->token(), $validator->errors(), $validator->errors(), "false");
+            }
+
+            else{
+                if ($validation->count() > 0) {
+                    return $this->respondWithToken($this->token(), 'Procedure Exception Already Exists', $validation, true, 200, 1);
+                }
+                $add_names = DB::table('PROCEDURE_EXCEPTION_NAMES')->insert(
+                    [
+                        'procedure_exception_list' => $request->procedure_exception_list,
+                        'exception_name'=>$request->exception_name,
+                        
+                    ]
+                );
+    
+                $add = DB::table('PROCEDURE_EXCEPTION_LISTS')
+                ->insert(
+                    [
+                        'procedure_exception_list'=>$request->procedure_exception_list,
+                        'accum_bene_strategy_id'=>$request->accum_bene_strategy_id,
+                        'benefit_code'=>$request->benefit_code,
+                        'copay_strategy_id'=>$request->copay_strategy_id,
+                        'coverage_start_days'=>$request->coverage_start_days,
+                        'diagnosis_id'=>$request->diagnosis_id,
+                        'diagnosis_list'=>$request->diagnosis_list,
+                        'max_price'=>$request->max_price,
+                        'max_price_opt'=>$request->max_price_opt,
+                        'module_exit'=>$request->module_exit,
+                        'new_claim_status'=>$request->new_claim_status,
+                        'physician_list'=>$request->physician_list,
+                        'physician_specialty_list'=>$request->physician_specialty_list,
+                        'pricing_strategy_id'=>$request->pricing_strategy_id,
+                        'proc_code_list_id'=>$request->proc_code_list_id,
+                        'process_rule'=>$request->process_rule,
+                        'provider_type'=>$request->provider_type,
+                        'reject_only_msg_flag'=>$request->reject_only_msg_flag,
+                        'rx_qty_opt_multiplier'=>$request->rx_qty_opt_multiplier,
+                        'service_modifier'=>$request->service_modifier,
+                        'service_type'=>$request->service_type,
+                        'sex_restriction'=>$request->sex_restriction,
+                        'valid_relation_code'=>$request->valid_relation_code,
+                        'effective_date' => $request->effective_date,
+                        'termination_date' => $request->termination_date,
+                        'message_stop_date' => $request->message_stop_date,
+                        'message'=>$request->message,
+                        'min_price'=>$request->min_price,
+                        'max_age'=>$request->max_age,
+                        'min_age'=>$request->min_age,
+                        'max_qty_over_time'=>$request->max_qty_over_time,
+                        'ucr'=>$request->ucr,
+                    ]);
+                   
+    
+                $add = DB::table('PROCEDURE_EXCEPTION_LISTS')->where('procedure_exception_list', 'like', '%' . $request->procedure_exception_list . '%')->first();
+                return $this->respondWithToken($this->token(), 'Record Added Successfully', $add);
+
+            }
+
+
+           
+        } else if ($request->add_new == 0) {
+
+            $validator = Validator::make($request->all(), [
+
+                "exception_name" => ['max:36'],
+                "NEW_CLAIM_STATUS"=>['max:2'],
+                "SERVICE_TYPE"=>['max:1'],
+                'PROCESS_RULE'=>['max:15','min:5'],
+                'PHYSICIAN_LIST'=>['max:10'],
+                'PHYSICIAN_SPECIALTY_LIST'=>['max:10'],
+                'DIAGNOSIS_LIST'=>['max:10'],
+                'PRICING_STRATEGY_ID'=>['max:11'],
+                'ACCUM_BENE_STRATEGY_ID'=>['max:11'],
+                'COPAY_STRATEGY_ID'=>['max:10'],
+                'MESSAGE'=>['max:10'],
+                'MESSAGE_STOP_DATE'=>['max:10'],
+                'MIN_AGE'=>['max:6'],
+                'MAX_AGE'=>['max:6'],
+                'MIN_PRICE'=>['max:6'],
+                'MAX_PRICE'=>['max:6'],
+                'MIN_PRICE_OPT'=>['max:6'],
+                'MAX_PRICE_OPT'=>['max:6'],
+                'VALID_RELATION_CODE'=>['max:6'],
+                'SEX_RESTRICTION'=>['max:6'],
+                'MODULE_EXIT'=>['max:6'],
+                'DATE_TIME_CREATED'=>['max:6'],
+                'USER_ID_CREATED'=>['max:6'],
+                'DATE_TIME_MODIFIED'=>['min:2','max:12'],
+                'USER_ID'=>['min:2','max:12'],
+                'EFFECTIVE_DATE'=>['max:6'],
+                'TERMINATION_DATE'=>['max:12','min:2'],
+                'REJECT_ONLY_MSG_FLAG'=>['max:12','min:2'],
+                'MAX_QTY_OVER_TIME'=>['max:12|min:2'],
+                'COVERAGE_START_DAYS'=>['max:6'],
+                'BENEFIT_CODE'=>['max:6'],
+                'SERVICE_MODIFIER'=>['max:2'],
+                'DIAGNOSIS_ID'=>['max:1'],
+                'MAX_RX_QTY_OPT'=>['max:1'],
+                'PROVIDER_TYPE'=>['max:1'],
+                'UCR'=>['numeric|max:6'],
+                'PROC_CODE_LIST_ID'=>['numeric|max:6'],
+                'RX_QTY_OPT_MULTIPLIER'=>['numeric|max:6'],
+
+
+            ]);
+
+            if ($validator->fails()) {
+                return $this->respondWithToken($this->token(), $validator->errors(), $validator->errors(), "false");
+            }
+
+            else{
+
+                // if ($validation->count() < 1) {
+                //     return $this->respondWithToken($this->token(), 'Record Not Found', $validation, false, 404, 0);
+                // }
+    
+                $update_names = DB::table('PROCEDURE_EXCEPTION_NAMES')
+                ->where('procedure_exception_list', $request->procedure_exception_list )
+                ->first();
+                    
+    
+                $checkGPI = DB::table('PROCEDURE_EXCEPTION_LISTS')
+                ->where('procedure_exception_list',$request->procedure_exception_list)
+                ->where('proc_code_list_id',$request->proc_code_list_id)
+                ->where('service_modifier',$request->service_modifier)
+                ->where('benefit_code',$request->benefit_code)
+                ->where('diagnosis_list',$request->diagnosis_list)
+                ->where('provider_type',$request->provider_type)
+                ->where('service_type',$request->service_type)
+                ->where('effective_date',$request->effective_date)
+
+                    ->get()
+                    ->count();
+                    // dd($checkGPI);
+                // if result >=1 then update NDC_EXCEPTION_LISTS table record
+                //if result 0 then add NDC_EXCEPTION_LISTS record
+
+    
+                if ($checkGPI <= "0") {
+                    $update = DB::table('PROCEDURE_EXCEPTION_LISTS')
+                    ->insert(
+                        [
+                            'procedure_exception_list'=>$request->procedure_exception_list,
+                            'accum_bene_strategy_id'=>$request->accum_bene_strategy_id,
+                            'benefit_code'=>$request->benefit_code,
+                            'copay_strategy_id'=>$request->copay_strategy_id,
+                            'coverage_start_days'=>$request->coverage_start_days,
+                            'diagnosis_id'=>$request->diagnosis_id,
+                            'diagnosis_list'=>$request->diagnosis_list,
+                            'max_price'=>$request->max_price,
+                            'max_price_opt'=>$request->max_price_opt,
+                            'module_exit'=>$request->module_exit,
+                            'new_claim_status'=>$request->new_claim_status,
+                            'physician_list'=>$request->physician_list,
+                            'physician_specialty_list'=>$request->physician_specialty_list,
+                            'pricing_strategy_id'=>$request->pricing_strategy_id,
+                            'proc_code_list_id'=>$request->proc_code_list_id,
+                            'process_rule'=>$request->process_rule,
+                            'provider_type'=>$request->provider_type,
+                            'reject_only_msg_flag'=>$request->reject_only_msg_flag,
+                            'rx_qty_opt_multiplier'=>$request->rx_qty_opt_multiplier,
+                            'service_modifier'=>$request->service_modifier,
+                            'service_type'=>$request->service_type,
+                            'sex_restriction'=>$request->sex_restriction,
+                            'valid_relation_code'=>$request->valid_relation_code,
+                            'effective_date' => $request->effective_date,
+                            'termination_date' => $request->termination_date,
+                            'message_stop_date' => $request->message_stop_date,
+                            'message'=>$request->message,
+                            'min_price'=>$request->min_price,
+                            'max_age'=>$request->max_age,
+                            'min_age'=>$request->min_age,
+                            'max_qty_over_time'=>$request->max_qty_over_time,
+                            'ucr'=>$request->ucr,
+                        ]);
+                    
+
+                $update = DB::table('PROCEDURE_EXCEPTION_LISTS')->where('procedure_exception_list', 'like', '%' . $request->procedure_exception_list . '%')->first();
+                return $this->respondWithToken($this->token(), 'Record Added Successfully', $update);
+
+                } else {
+  
+
+                    $add_names = DB::table('PROCEDURE_EXCEPTION_NAMES')
+                    ->where('procedure_exception_list',$request->procedure_exception_list)
+                    ->update(
+                        [
+                            'exception_name'=>$request->exception_name,
+                            
+                        ]
+                    );
+
+                    $update = DB::table('PROCEDURE_EXCEPTION_LISTS' )
+                    ->where('procedure_exception_list',$request->procedure_exception_list)
+                ->where('proc_code_list_id',$request->proc_code_list_id)
+                ->where('service_modifier',$request->service_modifier)
+                ->where('benefit_code',$request->benefit_code)
+                ->where('diagnosis_list',$request->diagnosis_list)
+                ->where('provider_type',$request->provider_type)
+                ->where('service_type',$request->service_type)
+                ->where('effective_date',$request->effective_date)
+                    ->update(
+                        [
+                            'accum_bene_strategy_id'=>$request->accum_bene_strategy_id,
+                            'copay_strategy_id'=>$request->copay_strategy_id,
+                            'coverage_start_days'=>$request->coverage_start_days,
+                            'diagnosis_list'=>$request->diagnosis_list,
+                            'max_age'=>$request->max_age,
+                            'max_price'=>$request->max_price,
+                            'max_price_opt'=>$request->max_price_opt,
+                            'module_exit'=>$request->module_exit,
+                            'new_claim_status'=>$request->new_claim_status,
+                            'physician_list'=>$request->physician_list,
+                            'physician_specialty_list'=>$request->physician_specialty_list,
+                            'pricing_strategy_id'=>$request->pricing_strategy_id,
+                            'proc_code_list_id'=>$request->proc_code_list_id,
+                            'process_rule'=>$request->process_rule,
+                            'reject_only_msg_flag'=>$request->reject_only_msg_flag,
+                            'rx_qty_opt_multiplier'=>$request->rx_qty_opt_multiplier,
+                            'sex_restriction'=>$request->sex_restriction,
+                            'valid_relation_code'=>$request->valid_relation_code,
+                            'termination_date' => $request->termination_date,
+                            'message_stop_date' => $request->message_stop_date,
+                            'message'=>$request->message,
+                            'min_price'=>$request->min_price,
+                            'min_age'=>$request->min_age,
+                            'max_qty_over_time'=>$request->max_qty_over_time,
+                            'ucr'=>$request->ucr,
+                            
+        
+                        ]
+                    );
+                    $update = DB::table('PROCEDURE_EXCEPTION_LISTS')->where('procedure_exception_list', 'like', '%' . $request->ndc_exception_list . '%')->first();
+                    return $this->respondWithToken($this->token(), 'Record Updated Successfully', $update);
+                }
+    
+               
+
+            }
+
+           
+        }
     }
 
     public function AllPhysicainLists(Request $request){
