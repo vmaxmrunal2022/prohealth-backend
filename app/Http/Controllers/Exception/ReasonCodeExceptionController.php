5ee3f6987f49187fe3915bb141e01d636f6ca53c
vmaxmahesh <vmax.mahesh@gmail.com>
2023-04-29 15:51:17 +0530

    provider type proc

--- app/Http/Controllers/Exception/ReasonCodeExceptionController.php
+++ app/Http/Controllers/Exception/ReasonCodeExceptionController.php
@@ -12,100 +12,100 @@ class ReasonCodeExceptionController extends Controller
 
 
 
-    public function addcopy(Request $request)
-    {
+    // public function addcopy(Request $request)
+    // {
 
 
 
-        $createddate = date('y-m-d');
-        $effective_date = date('Ymd', strtotime($request->effective_date));
-        $terminate_date = date('Ymd', strtotime($request->termination_date));
+    //     $createddate = date('y-m-d');
+    //     $effective_date = date('Ymd', strtotime($request->effective_date));
+    //     $terminate_date = date('Ymd', strtotime($request->termination_date));
 
 
-        $check = DB::table('REASON_CODE_LISTS')
-            ->where('REASON_CODE_LIST_ID', strtoupper($request->reason_code_list_id))
-            ->first();
+    //     $check = DB::table('REASON_CODE_LISTS')
+    //         ->where('REASON_CODE_LIST_ID', strtoupper($request->reason_code_list_id))
+    //         ->first();
 
 
-        if ($request->has('new')) {
+    //     if ($request->has('new')) {
 
-            if ($check) {
-                return $this->respondWithToken($this->token(), 'Reason Code List Id Already Exists', $check);
+    //         if ($check) {
+    //             return $this->respondWithToken($this->token(), 'Reason Code List Id Already Exists', $check);
 
-            } else {
+    //         } else {
 
-                $accum_benfit_stat = DB::table('REASON_CODE_LISTS')
-                ->insert(
-                    [
-                        'reason_code_list_id' => strtoupper($request->reason_code_list_id),
-                        'reject_code' => $request->reject_code,
-                        'reason_code' => $request->reason_code,
-                        'effective_date' => $effective_date,
-                        'termination_date' => $terminate_date,
-                        'date_time_created' => $createddate,
-                        'date_time_modified' => $createddate,
-                    ]
-                );
-
-
-                $accum_benfit_stat = DB::table('REASON_CODE_LIST_NAMES')->insert(
-                    [
-
-                        'reason_code_list_id' => strtoupper($request->reason_code_list_id),
-                        'reason_code_name' => $request->reason_code_name,
-                        'date_time_created' => $createddate,
+    //             $accum_benfit_stat = DB::table('REASON_CODE_LISTS')
+    //             ->insert(
+    //                 [
+    //                     'reason_code_list_id' => strtoupper($request->reason_code_list_id),
+    //                     'reject_code' => $request->reject_code,
+    //                     'reason_code' => $request->reason_code,
+    //                     'effective_date' => $effective_date,
+    //                     'termination_date' => $terminate_date,
+    //                     'date_time_created' => $createddate,
+    //                     'date_time_modified' => $createddate,
+    //                 ]
+    //             );
 
 
-                    ]
-                );
+    //             $accum_benfit_stat = DB::table('REASON_CODE_LIST_NAMES')->insert(
+    //                 [
 
-                $benefitcode = DB::table('REASON_CODE_LISTS')->where('reason_code_list_id', 'like', $request->reason_code_list_id)->first();
-
-                return $this->respondWithToken($this->token(), 'Record Added Successfully', $benefitcode);
-
-            }
+    //                     'reason_code_list_id' => strtoupper($request->reason_code_list_id),
+    //                     'reason_code_name' => $request->reason_code_name,
+    //                     'date_time_created' => $createddate,
 
 
+    //                 ]
+    //             );
 
+    //             $benefitcode = DB::table('REASON_CODE_LISTS')->where('reason_code_list_id', 'like', $request->reason_code_list_id)->first();
 
+    //             return $this->respondWithToken($this->token(), 'Record Added Successfully', $benefitcode);
 
-        } else {
+    //         }
 
 
 
 
 
-            $createddate = DB::table('REASON_CODE_LISTS')
-                ->where('reason_code_list_id', $request->reason_code_list_id)
-                ->update(
-                    [
-                        'reject_code' => $request->reject_code,
-                        'reason_code' => $request->reason_code,
-                        'effective_date' => $effective_date,
-                        'termination_date' => $terminate_date,
-                        'date_time_created' => $createddate,
-                        'date_time_modified' => $createddate,
-
-                    ]
-                );
+    //     } else {
 
 
-            $reason_code_names = DB::table('REASON_CODE_LIST_NAMES')
-                ->where('reason_code_list_id', $request->reason_code_list_id)
-                ->update(
-                    [
-                        'reason_code_name' => $request->reason_code_name,
-
-                    ]
-                );
-
-            $benefitcode = DB::table('REASON_CODE_LIST_NAMES')->where('reason_code_name', 'like', '%' . $request->reason_code_name . '%')->first();
-
-        }
 
 
-        return $this->respondWithToken($this->token(), 'Record Updated Successfully', $benefitcode);
-    }
+
+    //         $createddate = DB::table('REASON_CODE_LISTS')
+    //             ->where('reason_code_list_id', $request->reason_code_list_id)
+    //             ->update(
+    //                 [
+    //                     'reject_code' => $request->reject_code,
+    //                     'reason_code' => $request->reason_code,
+    //                     'effective_date' => $effective_date,
+    //                     'termination_date' => $terminate_date,
+    //                     'date_time_created' => $createddate,
+    //                     'date_time_modified' => $createddate,
+
+    //                 ]
+    //             );
+
+
+    //         $reason_code_names = DB::table('REASON_CODE_LIST_NAMES')
+    //             ->where('reason_code_list_id', $request->reason_code_list_id)
+    //             ->update(
+    //                 [
+    //                     'reason_code_name' => $request->reason_code_name,
+
+    //                 ]
+    //             );
+
+    //         $benefitcode = DB::table('REASON_CODE_LIST_NAMES')->where('reason_code_name', 'like', '%' . $request->reason_code_name . '%')->first();
+
+    //     }
+
+
+    //     return $this->respondWithToken($this->token(), 'Record Updated Successfully', $benefitcode);
+    // }
 
     public function add(Request $request)
     {
