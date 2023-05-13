680a14a5791a7dea71da6726cdd4a2425ed232c4
vmaxmrunal2022 <110972208+vmaxmrunal2022@users.noreply.github.com>
2023-05-13 10:36:28 +0530

    user data completed

--- app/Http/Controllers/AccumLatedBenifits/MajorMedicalController.php
+++ app/Http/Controllers/AccumLatedBenifits/MajorMedicalController.php
@@ -93,9 +93,8 @@ class MajorMedicalController extends Controller
 
     public function getClientGroup($ndcid)
     {
-
         $ndc = DB::table('CLIENT_GROUP')
-            ->where('CLIENT_ID', 'like', '%' . $ndcid . '%')
+            ->where(DB::raw('UPPER(CLIENT_GROUP_ID)'), 'like', '%' . strtoupper($ndcid) . '%')
             ->get();
 
         return $this->respondWithToken($this->token(), '', $ndc);
